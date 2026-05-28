4f7c007dcb5bfc9a6560c611d0f599d0f5e0ac91
jerisharand <jerisharand@gmail.com>
2026-05-28 11:11:43 +0530

    Update pointers_in_arrays.cpp

--- simple/pointers_in_arrays.cpp
+++ simple/pointers_in_arrays.cpp
@@ -4,8 +4,9 @@ int main() {
     using namespace std;
     int x[] = {1,2,3,4,5};
     int *p = &x[0];
-cout << p[0];
+    cout << p[0];
     cout << *p << " " << x[0] << endl;
     p = p + 1;
     cout << *p << endl;
     return 0;
+}
