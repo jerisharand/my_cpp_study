619cc544e7ee53d497e5af45cd48bf7b83f58668
jerisharand <jerisharand@gmail.com>
2026-05-28 11:05:36 +0530

    Create pointers_in_arrays.cpp

---
+++ simple/pointers_in_arrays.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+
+int main() {
+    using namespace std;
+    int x[] = {1,2,3,4,5};
+    int *p = &x[0];
+cout << p[0];
+    cout << *p << " " << x[0] << endl;
+    p = p + 1;
+    cout << *p << endl;
+    return 0;
