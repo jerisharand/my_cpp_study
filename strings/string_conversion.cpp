e7ced63485d2fd02fbaddd5b53efc6248ee98eac
Jerisha Rand Reemus <jerisharand@gmail.com>
2026-07-10 12:02:14 +0530

    Create string_conversion.cpp

---
+++ strings/string_conversion.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout <<  std::stoi("1234") << std::endl;
+	std::cout <<  std::stof("12.3") << std::endl;
+	std::cout <<  std::stod("12.34") << std::endl;
+	std::cout <<  std::stol("12.3456") << std::endl;
+}
